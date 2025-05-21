0a4cefd87d0f5f0c0eaac30be637633e1ed75331
subkek <100521725+sub-kek@users.noreply.github.com>
2025-05-21 14:33:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
 plugins {
     `java-library`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.8"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-}
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(11))
-    withSourcesJar()
-}
-
-if (properties.containsKey("snapshot")) {
-    version = "$version-SNAPSHOT"
 }
 
 repositories {
@@ -33,58 +24,52 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 }
 
-tasks.shadowJar {
-    configurations = listOf(project.configurations.shadow.get())
-    mergeServiceFiles()
-
-    archiveBaseName.set(rootProject.name)
-    archiveClassifier.set("")
-    archiveAppendix.set("")
-
-    relocate("org.apache", "io.github.subkek.lavaplayer.libs.org.apache")
-    relocate("org.jsoup", "io.github.subkek.lavaplayer.libs.org.jsoup")
-    relocate("com.fasterxml", "io.github.subkek.lavaplayer.libs.com.fasterxml")
-    relocate("net.iharder", "io.github.subkek.lavaplayer.libs.net.iharder")
-    relocate("ibxm", "io.github.subkek.lavaplayer.libs.ibxm")
-    relocate("net.sourceforge", "io.github.subkek.lavaplayer.libs.net.sourceforge")
-    relocate("org.json", "io.github.subkek.lavaplayer.libs.org.json")
-    relocate("org.intellij", "io.github.subkek.lavaplayer.libs.org.intellij")
-    relocate("org.jetbrains", "io.github.subkek.lavaplayer.libs.org.jetbrains")
-
-    relocate("dev.lavalink", "io.github.subkek.lavaplayer.libs.dev.lavalink")
-    relocate("com.grack", "io.github.subkek.lavaplayer.libs.com.grack")
-    relocate("com.github.topi314", "io.github.subkek.lavaplayer.libs.com.github.topi314")
-    relocate("com.auth0", "io.github.subkek.lavaplayer.libs.com.auth0")
-    relocate("dev.schlaubi", "io.github.subkek.lavaplayer.libs.dev.schlaubi")
-
-    exclude("lavalink-plugins/**")
-
-    relocate("com.sedmelluq", "io.github.subkek.lavaplayer.libs.com.sedmelluq") {
-        exclude("com/sedmelluq/discord/lavaplayer/natives/**")
-    }
-    relocate("mozilla", "io.github.subkek.lavaplayer.libs.mozilla")
-    relocate("certificates", "io.github.subkek.lavaplayer.libs.certificates")
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    disableAutoTargetJvm()
 }
 
-bukkit {
-    name = rootProject.name
-    version = rootProject.version as String
-    main = "io.github.subkek.lavaplayer.CdLib"
-
-    authors = listOf(
-        "subkek",
-        "yiskii"
-    )
-    website = "https://discord.gg/eRvwvmEXWz"
-    apiVersion = "1.16"
-
-    foliaSupported = true
-
-    depend = listOf(
-        "CustomDiscs",
-    )
+tasks.jar {
+    enabled = false
 }
 
 tasks.build {
     dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    archiveFileName = "${rootProject.name}-$version.jar"
+
+    configurations = listOf(project.configurations.shadow.get())
+    mergeServiceFiles()
+
+    fun relocate(pkg: String) = relocate(pkg, "${rootProject.group}.customdiscs.libs.$pkg") {
+        exclude("com/sedmelluq/discord/lavaplayer/natives/**")
+    }
+
+    relocate("org.apache")
+    relocate("org.jsoup")
+    relocate("com.fasterxml")
+    relocate("org.yaml.snakeyaml")
+    relocate("org.simpleyaml")
+    relocate("org.jflac")
+    relocate("org.json")
+    relocate("org.tritonus")
+    relocate("mozilla")
+    relocate("junit")
+    relocate("javazoom")
+    relocate("certificates")
+    relocate("org.hamcrest")
+    relocate("org.junit")
+    relocate("net.sourceforge.jaad.aac")
+    relocate("net.kyori")
+    relocate("net.iharder")
+    relocate("com.tcoded")
+    relocate("com.grack")
+    relocate("dev.lavalink")
+    relocate("org.intellij")
+    relocate("org.jetbrains")
+    relocate("com.sedmelluq")
+    relocate("dev.jorel.commandapi")
 }
